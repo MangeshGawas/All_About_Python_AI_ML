cdb9f87ad0dd850edac77230221ceea6cac57ac4
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-08 22:35:08 +0530

    appendMethod added

---
+++ LinkedList.py
@@ -0,0 +1,27 @@
+#Create a Node
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self,data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            last_node = self.head
+            while last_node.next:
+                last_node = last_node.next
+            last_node.next = new_node
+
+ 
+    
+
+linkedList1 = LinkedList()
+print(linkedList1)
+        
