621d103cc3231e6bd73f809465938443326737f8
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-16 00:25:49 +0530

    PythonLearning

---
+++ python108/pythonDay_1.py
@@ -0,0 +1,31 @@
+#Introduction
+'''
+Python is the high level interpreted programming language, simple to read.Developed by Guido avn Rossum in 1991
+1)Easy to learn
+2)versatility
+3)Interpretedd and interactive
+4)Large Standard Library
+5)Large community support
+'''
+
+#Lets Start Day 1
+
+#print Statement
+print("hello World")
+
+#String Manipulation
+message = "Hello"
+name = "Bond"
+full_message = message + "," + name + "!"
+print(full_message)
+
+#input Variable
+name = input("Enter your Name:")
+print("Hello,"+ name + "!")
+
+
+# Day 1 Project Idea(Personalized Greeting)
+name = input("Enter the person name :")
+greeting_message= "Hello, "+ name + "!welcome to Python Day 1"
+
+print(greeting_message)
