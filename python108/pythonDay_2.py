7743851dfbc11c572632594c6f4e7dd48d7327a3
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-17 00:09:35 +0530

    Day2

---
+++ python108/pythonDay_2.py
@@ -0,0 +1,20 @@
+# Day 2 is more like understanding data type, string manipulation and one small mini project
+
+#Integer
+x = 5
+y = -10
+
+#floats
+pi = 3.14
+temperature = -0.5
+
+#String
+name = "John"
+message = "Hello ,World"
+
+
+print(x ,end="\n")
+print(y ,end="\n")
+print(temperature ,end="\n")
+
+
