8371bb203c641a3ddb3ca6059806004dd92b404e
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-17 00:15:40 +0530

    all data types in python

--- python108/pythonDay_2.py
+++ python108/pythonDay_2.py
@@ -16,5 +16,25 @@ message = "Hello ,World"
 print(x ,end="\n")
 print(y ,end="\n")
 print(temperature ,end="\n")
+print(message,end="\n")
+
+#Boolean
+is_python_fun = True
+is_raining = False
+
+#List
+numbers = [1,2,3,4,5,6]
+fruits = ["Apple","Banana", "Cherry"]
+
+#Tuples
+point=(10,20)
+
+#Dictionaries
+person = {"name":"Amit","age":24}
+
+#Sets
+unique_numbers = {1,2,3,4,5} #there is no duplicate value present in the set
+
+
 
 
