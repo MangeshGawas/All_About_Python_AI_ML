1442b46c63dfc4f999516c817b5282ba8e9fbd9e
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-18 01:14:37 +0530

    day 3 commit

---
+++ python108/pythonDay_3.py
@@ -0,0 +1,27 @@
+#Lets begins with the day 3
+#Control flow, logical operator and mini Project /project
+
+#Control Flow statement if, if else, if elif else
+
+#if
+x = 5
+if x > 0:
+    print('x is positive')
+
+
+#if else
+y = -3
+if y >=0:
+    print('y is non negative')
+else:
+    print("y is negative")
+
+#if elif else
+z = 0
+if  z < 0:
+    print("z is negative number")
+elif z > 0:
+    print('z is positive number')
+else:
+    print('z is 0')
+    
