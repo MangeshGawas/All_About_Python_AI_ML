4a63fa619b7278f0476050481286e44098836e8b
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-20 23:16:29 +0530

    Day5

---
+++ python108/pythonDay_5.py
@@ -0,0 +1,15 @@
+#Lets begin the python day 5 : Python Loop
+
+#Python basically have 2 types of loop ie for and while loop
+
+#for Loop
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+#while Loop
+#loop beginning with 0 because i = 0 , ie like initializing the 
+i = 0
+while i <= 5:
+    print(i)
+    i = i+1
