3aab47a50ac3da074b495c37d4b6a9e90bdbae8d
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-23 23:37:31 +0530

    Day 8

---
+++ python108/pythonDay_8.py
@@ -0,0 +1,20 @@
+#Day 9 : Dictionaries , nesting and the secret Auction
+
+#create Dictionaries {}
+person = {"name":"Alice","age":30, "city":"new york"}
+
+print(person["name"])
+
+#adding and modifying elements
+person["email"] = "alice@gmail.com"
+person["age"] = 31
+
+'''Dictionary Methods: Python provides various built-in methods to manipulate dictionaries, such as 
+keys(), values(), items(), pop(), popitem(), clear(), update(), etc.
+'''
+
+#dictionary methods
+print(person.keys())
+print(person.values())
+
+
