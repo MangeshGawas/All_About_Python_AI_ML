03fb0ea0c6f7b47edfbd7fe7a40b7935e65fad59
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-28 16:38:58 +0530

    commit

--- python108/pythonDay_11.py
+++ python108/pythonDay_11.py
@@ -7,6 +7,12 @@ add = lambda x, y : x+y
 print(add(12,3))
 
 #map function 
-numbers = [1,2,3,4,5]
+numbers = [1,2,3,4,5,6]
 squared = map(lambda x: x**2, numbers)
 print(list(squared))
+
+#filters
+even_number = filter(lambda x: x%2==0, numbers)
+print(list(even_number))
+
+#sorted
