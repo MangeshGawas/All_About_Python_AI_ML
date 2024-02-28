6730b9cfee95c1b3725749f00be30fb1f3ea4134
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-28 17:11:35 +0530

    more examples

--- python108/pythonDay_12.py
+++ python108/pythonDay_12.py
@@ -11,3 +11,10 @@ print(squares,"output from tranditional loop")
 squares2 = [x**2 for x in range(1,6)]
 print(squares2, "using list comprehension")
 
+#Using LC to filter even number
+evens = [x for x in range(1,11) if x%2 == 0]
+print(evens, "Even number")
+
+#Using list comprehension with nested loops
+matrix = [[i * j for j in range(1, 4)] for i in range(1, 4)]
+print(matrix)
