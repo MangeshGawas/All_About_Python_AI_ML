b1f5f68c38e3ec0247e059b2b698cd6b4a86a376
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-28 16:11:10 +0530

    Day10 Recursion

--- python108/pythonDay_10.py
+++ python108/pythonDay_10.py
@@ -1,1 +1,21 @@
-#day 10 is sunday
+#day 10 is Recurssion
+#Factorial
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+result = factorial(4)
+print(result)
+
+#fibonacci
+def fibonacci(n):
+    if n<=1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+print(fibonacci(7))
+
+
