04a43db37c8410a0a310e9e67c85f359f32502bc
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-28 16:17:13 +0530

    binary search in recursive

--- python108/pythonDay_10.py
+++ python108/pythonDay_10.py
@@ -18,4 +18,23 @@ def fibonacci(n):
     
 print(fibonacci(7))
 
+#binary search
+def binary_search(arr, target, low , high):
+    if low > high:
+        return -1
+    mid = (low + high) // 2
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return binary_search(arr, target, mid+1, high)
+    else:
+        return binary_search(arr, target, low, mid-1)
+    
+
+arr = [1,2,3,4,5,6,7,8]
+print(binary_search(arr, 5,0,len(arr)-1))
+
+
+
+
 
