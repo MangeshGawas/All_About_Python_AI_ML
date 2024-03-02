de18c36191387432a1e9519e08b084a3b0a5f802
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-02 23:43:22 +0530

    list method

--- python108/pythonDay_14.py
+++ python108/pythonDay_14.py
@@ -6,3 +6,16 @@ mixed_list = [1,"hello", 3.14, True]
 
 print(my_list[0])
 print(my_list[3])
+print(my_list[-1])
+print(my_list[-2])
+
+my_list.append(111)
+last_element = my_list.pop()
+print(last_element)
+
+my_list.insert(2, 777)
+print(my_list)
+
+my_new_list = [1,12,2,3,4,5,3]
+my_new_list.remove(3)
+print(my_new_list)
