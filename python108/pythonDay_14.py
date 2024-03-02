80db213e825493502e985e4cdd40f68ee766fc48
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-02 23:25:57 +0530

    day 14

---
+++ python108/pythonDay_14.py
@@ -0,0 +1,8 @@
+# Day 16: Introduction to Lists - Creation, Accessing Elements, and List Methods
+
+my_list = [1,2,3,4,5]
+
+mixed_list = [1,"hello", 3.14, True]
+
+print(my_list[0])
+print(my_list[3])
