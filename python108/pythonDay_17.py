ffee960bbda67400674807d2be7dbd3220fd467c
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-03 23:49:47 +0530

    day19 tuple

---
+++ python108/pythonDay_17.py
@@ -0,0 +1,14 @@
+# Day 19 : Introduction to tuple
+#creation of tuple
+my_tuple = (1,2,3,4,5)
+mixed_tuple = (1,"hello",3.14,True)
+
+
+#accessing Element
+my_tuple = (10,20,30,40,50)
+print(my_tuple[0])
+print(my_tuple[2])
+
+print(my_tuple[-1])
+print(my_tuple[-2])
+
