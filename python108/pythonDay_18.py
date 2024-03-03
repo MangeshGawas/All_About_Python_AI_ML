9b8c1b3b8375b55ffc1c8a57f662b760c46e0dec
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-03 23:57:51 +0530

    Day18

---
+++ python108/pythonDay_18.py
@@ -0,0 +1,3 @@
+# What are Immutable Data Structures?
+
+# Immutable data structures are data structures whose state cannot be modified after they are created. Once created, the elements within an immutable data structure cannot be changed, added, or removed. Tuples in Python are a prime example of immutable data structures.
