39ace71c9ceaf2a8e957931762439fc9ec2cc173
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-08 23:17:46 +0530

    day24

---
+++ python108/pythonDay_24.py
@@ -0,0 +1,17 @@
+#Day 24 : String Manipulation - slicing, concatenation and formatting
+s = "Hello , World"
+substring = s[3:8]
+print(substring)
+
+#concatenating String
+s1 = "Hello"
+s2 = "world"
+concatenated_string = s1+ "," + s2 +"!"
+print(concatenated_string)
+
+#join
+
+word = ["Hello","world"]
+concatenated_string = " ".join(word)
+print(concatenated_string)
+
