b6d541861fd95ab3580c28bc8cc54954f9a2bb3b
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-13 23:44:40 +0530

    Day 31 Subclassing and Method overloading

---
+++ python108/pythonDay_31.py
@@ -0,0 +1,14 @@
+# Day 31: Inheritance ,Subclassing and method overloading
+
+# sublclassing
+class Animal:
+    def sound(self):
+        return "Some generic sound"
+    
+class Dog:
+    def bark(self):
+        return "Woof!"
+
+class Cat:
+    def sound(self): # method overloading Animals
+        return "Meow!"
