28dc326c6eb980687327e65715f26ee1d98d596c
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-14 14:05:24 +0530

    PythonOOP

---
+++ turtle/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen #from package turtle importing Turtle and screen class
+
+
+timmy = Turtle() #object created 
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+#object attribute
+my_screen = Screen() #constructor
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
