e71241a3f05144e0999ebb30dacced827333c71b
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-15 12:48:48 +0530

    used contructor to the class and for that particular user added one follower count

--- oop-start/main.py
+++ oop-start/main.py
@@ -1,16 +1,31 @@
 class User:
-    pass
+    def __init__(self,user_id, username):
+        self.user_id = user_id
+        self.username = username
+        self.followers = 0
+    
+    def add_follower(self):
+        self.followers = self.followers+1
+
+        
+        
+        
 
 
 
-user_1 = User()
+user_1 = User("001","Tom")
 
-user_1.id = "001"
-user_1.username = "Tom"
+# user_1.id = "001"
+# user_1.username = "Tom"
 print(user_1.username)
 
+print(user_1.followers, "Before add follower class call")
 
-user_2 = User()
-user_2.id = "002"
-user_2.username = "Nathon"
-print(user_2.username)
+user_1.add_follower()
+
+print(user_1.followers,"After adding follower")
+
+# user_2 = User()
+# user_2.id = "002"
+# user_2.username = "Nathon"
+# print(user_2.username)
