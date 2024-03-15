48263ec818168cd8af2898c30326ca50395e4a2e
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-15 12:34:43 +0530

    class and attribute

---
+++ oop-start/main.py
@@ -0,0 +1,16 @@
+class User:
+    pass
+
+
+
+user_1 = User()
+
+user_1.id = "001"
+user_1.username = "Tom"
+print(user_1.username)
+
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "Nathon"
+print(user_2.username)
