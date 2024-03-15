5e7d8f870ee5bffa15124f9d2e867656ba88f1ba
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-15 12:52:50 +0530

    refactor the code

--- oop-start/main.py
+++ oop-start/main.py
@@ -7,11 +7,6 @@ class User:
     def add_follower(self):
         self.followers = self.followers+1
 
-        
-        
-        
-
-
 
 user_1 = User("001","Tom")
 
@@ -24,6 +19,7 @@ print(user_1.followers, "Before add follower class call")
 user_1.add_follower()
 
 print(user_1.followers,"After adding follower")
+
 
 # user_2 = User()
 # user_2.id = "002"
