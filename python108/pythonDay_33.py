d5818bf1f0932214ad63f8c053e0d4f2ea2af86f
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-16 22:45:20 +0530

    day 33

--- python108/pythonDay_33.py
+++ python108/pythonDay_33.py
@@ -9,7 +9,21 @@ class Car:
     def drive(self):
         return f"Driving {self._brand} {self.__model}"
     
+    #access modifier
+    @property
+    def model(self):
+        return self.__model
+    
+    @model.setter
+    def model(self,value):
+        self.__model = value
+    
 car1 = Car("Toyota", "Corolla")
 print(car1._brand) #It will be printing
-print(car1.__model) #Attribute error since model is a private attribute
+# print(car1.__model) #Attribute error since model is a private attribute
 
+#access modifier
+car = Car("Tata", "Nexon")
+print(car.model)
+car.model = "Tiago"
+print(car.model)
