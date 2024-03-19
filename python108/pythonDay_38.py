e7a654c44053771ce8e877fbe270457a0ec0c45c
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-03-19 22:15:07 +0530

    Day 38: Error handling

---
+++ python108/pythonDay_38.py
@@ -0,0 +1,32 @@
+print("try except")
+try:
+    x = 10/0
+except ZeroDivisionError:
+    print("Error:Division")
+
+
+#Handling multiple exception 
+print("Handling Multiple exception")
+try:
+    x = int("abc")
+except ValueError:
+    print("Error: Invalid conversion to int")
+except ZeroDivisionError:
+    print("Error: Division by Zero")
+
+#Handling any exception
+print("Handling any exception")
+try:
+    x = 10/0
+except Exception as e:
+    print("An error is ",e)
+
+#Finally Block
+print("Finally Block")
+try: 
+    x = 10/0
+except ZeroDivisionError:
+    print("Error:Division by zero")
+finally:
+    print("Clean up")
+
