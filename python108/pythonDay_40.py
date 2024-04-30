47cd8af71a05106720bcb0d78ab1b6a1917319da
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-04-30 23:33:04 +0530

    day 40

---
+++ python108/pythonDay_40.py
@@ -0,0 +1,15 @@
+def apply_operation(func,x,y):
+    return func(x,y)
+
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+result1 = apply_operation(add,3,5)
+result2 = apply_operation(subtract, 7,5)
+
+print(f"result1 ={result1}\nresult2={result2}")
+
+
