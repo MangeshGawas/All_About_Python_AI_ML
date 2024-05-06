ab91dfa0e95469bbc1065e67b0719a1e90616c92
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-05-06 15:50:08 +0530

    Create pythonDay_41.py
    day41

---
+++ python108/pythonDay_41.py
@@ -0,0 +1,33 @@
+# 1. Assign the value 10 to a variable x and print its value:
+
+x = 10 
+print(x)
+
+# 1. Create a list fruits with the values "apple", "banana", and "cherry", and print its elements:
+fruits = ["apple", "banana","cherry"]
+for fruit in fruits:
+    print(fruit)
+
+
+# 1. Define a function hello that takes a name as an argument and prints a greeting message. Call the function with the argument "Alice":
+def hello(name):
+    print(f"hello {name}")
+
+hello("Alice")
+
+# 1. Use an if-else statement to print "x is positive" if the value of x is greater than 0, and "x is negative" otherwise:
+x = 10
+
+if x<0:
+    print(x, "is a negative number")
+else:
+    print(x, "is a positive number")
+
+# 1. Write a for loop that prints the numbers from 1 to 5:
+for i in range(5):
+    print(i+1)
+
+for i in range(1, 6):
+    print(i)
+
+
