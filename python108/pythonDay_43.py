7b37d153754d06aefe25b508e68f932e09b2713a
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-05-08 18:39:01 +0530

    Update pythonDay_43.py
    regex all function and its uses

--- python108/pythonDay_43.py
+++ python108/pythonDay_43.py
@@ -9,3 +9,18 @@ if match:
 else:
     print("pattern not found")
 
+match_starting = re.match(pattern,text)
+if match:
+    print("pattern found at the start:", match.group())
+else:
+    print("Match not found")
+
+
+#find all occurance in the string
+text = "I love apples, apples are tasty"
+matches = re.findall(pattern, text)
+print("Occurances:",matches)
+
+#replace a pattern with a replacement
+new_text = re.sub(pattern, "banana", text)
+print("New text:",new_text)
