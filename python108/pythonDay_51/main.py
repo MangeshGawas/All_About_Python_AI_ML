84329e37a5de4bb57b7b9f3f61f2937434ecd86b
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-05-10 15:47:31 +0530

    Delete songdata.csv
    music recommendation

---
+++ python108/pythonDay_51/main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+import pickle
+
+# Load model
+df = pickle.load(open('df.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+def recommendation(song):
+    idx = df[df['song']==song].index[0]
+    distances = sorted(list(enumerate(similarity[idx])),reverse=False,key=lambda x:x[1])
+    songs = []
+    for i in distances[1:21]:
+        songs.append(df.iloc[i[0]].song)
+    return songs
+
+# App
+app = Flask(__name__)
+
+# Path
+@app.route('/')
+def index():
+    names = list(df['song'].values)
+    return render_template('index.html',name=names)
+
+@app.route('/recommend', methods=['POST'])
+@app.route('/recommend', methods=['POST'])
+def recom_song():
+    user_song = request.form['song']  # Changed key to 'song'
+    songs = recommendation(user_song)
+    return render_template('index.html', songs=songs)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
